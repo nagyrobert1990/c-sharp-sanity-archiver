3d3db68ac5b242e0056f7e9e0d456b0e2521664b
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-07-11 15:09:37 +0200

    base of the file browser is completed

--- SanityArchiver/Form1.cs
+++ SanityArchiver/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,38 @@ namespace SanityArchiver
 {
     public partial class Form1 : Form
     {
+        List<string> filesList = new List<string>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            filesList.Clear();
+            listView1.Items.Clear();
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select path" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string item in Directory.GetFiles(fbd.SelectedPath))
+                    {
+                        imageList1.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(item));
+                        FileInfo fileInfo = new FileInfo(item);
+                        filesList.Add(fileInfo.FullName);
+                        listView1.Items.Add(fileInfo.Name, imageList1.Images.Count - 1);
+
+                    }
+                }
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.FocusedItem != null)
+            {
+                Process.Start(filesList[listView1.FocusedItem.Index]);
+            }
         }
     }
 }
