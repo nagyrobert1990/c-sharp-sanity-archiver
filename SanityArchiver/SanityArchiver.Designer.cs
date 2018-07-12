ff6d164b298d99a9e6fc8d1a8d54b4532e6d57ac
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-07-12 11:35:37 +0200

    de/encryption, de/compress functions are ready

---
+++ SanityArchiver/SanityArchiver.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SanityArchiver
+{
+    partial class SanityArchiver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openButton = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.listView = new System.Windows.Forms.ListView();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.compressButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(713, 415);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(32, 32);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView
+            // 
+            this.listView.LargeImageList = this.imageList;
+            this.listView.Location = new System.Drawing.Point(13, 13);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(693, 396);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ListView_MouseClick);
+            this.listView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ListView_MouseDoubleClick);
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(12, 420);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(35, 13);
+            this.pathLabel.TabIndex = 2;
+            this.pathLabel.Text = "label1";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(53, 417);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(653, 20);
+            this.pathTextBox.TabIndex = 3;
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(712, 42);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 23);
+            this.encryptButton.TabIndex = 4;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // compressButton
+            // 
+            this.compressButton.Location = new System.Drawing.Point(712, 13);
+            this.compressButton.Name = "compressButton";
+            this.compressButton.Size = new System.Drawing.Size(75, 23);
+            this.compressButton.TabIndex = 5;
+            this.compressButton.Text = "Compress";
+            this.compressButton.UseVisualStyleBackColor = true;
+            this.compressButton.Click += new System.EventHandler(this.CompressButton_Click);
+            // 
+            // SanityArchiver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.compressButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.openButton);
+            this.Name = "SanityArchiver";
+            this.Text = "SanitiyArchiver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button compressButton;
+    }
+}
+
---
+++ SanityArchiver/SanityArchiver.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SanityArchiver
+{
+    public partial class SanityArchiver : Form
+    {
+        List<FileInfo> filesList = new List<FileInfo>();
+
+        public SanityArchiver()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            CreateFileList();
+        }
+
+        private void CreateFileList()
+        {
+            filesList.Clear();
+            listView.Items.Clear();
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select path" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    pathTextBox.Text = fbd.SelectedPath;
+                    foreach (string item in Directory.GetFiles(fbd.SelectedPath))
+                    {
+                        imageList.Images.Add(Icon.ExtractAssociatedIcon(item));
+                        FileInfo fileInfo = new FileInfo(item);
+                        filesList.Add(fileInfo);
+                        listView.Items.Add(fileInfo.Name, imageList.Images.Count - 1);
+                    }
+                }
+            }
+        }
+
+        private void ListView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView.FocusedItem != null)
+            {
+                Process.Start(filesList[listView.FocusedItem.Index].FullName);
+            }
+        }
+
+        private void ListView_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listView.FocusedItem != null)
+            {
+                FileInfo fi = filesList[listView.FocusedItem.Index];
+                if ((File.GetAttributes(fi.FullName)
+                    & FileAttributes.Hidden)
+                    != FileAttributes.Hidden & fi.Extension != ".gz")
+                {
+                    compressButton.Text = "Compress";
+                }
+                else
+                {
+                    compressButton.Text = "Decomress";
+                }
+
+                FileAttributes attr = File.GetAttributes(fi.FullName);
+                if ((attr & FileAttributes.Encrypted) == FileAttributes.Encrypted)
+                {
+                    encryptButton.Text = "Decrypt";
+                }
+                else
+                {
+                    encryptButton.Text = "Encrypt";
+                }
+            }
+        }
+
+        private void CompressButton_Click(object sender, EventArgs e)
+        {
+            if (listView.FocusedItem != null)
+            {
+                FileInfo fi = filesList[listView.FocusedItem.Index];
+                if (compressButton.Text == "Compress")
+                {
+                    Compress(fi);
+                }
+                else
+                {
+                    Decompress(fi);
+                }
+            }
+
+            filesList.Clear();
+            listView.Items.Clear();
+            ListFiles(Directory.GetFiles(pathTextBox.Text));
+        }
+
+        private void ListFiles(string[] files)
+        {
+            foreach (string item in files)
+            {
+                imageList.Images.Add(Icon.ExtractAssociatedIcon(item));
+                FileInfo fileInfo = new FileInfo(item);
+                filesList.Add(fileInfo);
+                listView.Items.Add(fileInfo.Name, imageList.Images.Count - 1);
+            }
+        }
+
+        private void Compress(FileInfo fi)
+        {
+            using (FileStream inFile = fi.OpenRead())
+            {
+                using (FileStream outFile =
+                            File.Create(fi.FullName + ".gz"))
+                {
+                    using (GZipStream Compress = new GZipStream(outFile, CompressionMode.Compress))
+                    {
+                        inFile.CopyTo(Compress);
+                    }
+                }
+            }
+        }
+
+        private void Decompress(FileInfo fi)
+        {
+            using (FileStream originalFileStream = fi.OpenRead())
+            {
+                string currentFileName = fi.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - fi.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                    }
+                }
+            }
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+            if (listView.FocusedItem != null)
+            {
+                if (encryptButton.Text == "Decrypt")
+                {
+                    DecryptFile(filesList[listView.FocusedItem.Index].FullName);
+                }
+                else
+                {
+                    EncryptFile(filesList[listView.FocusedItem.Index].FullName);
+                }
+            }
+
+            filesList.Clear();
+            listView.Items.Clear();
+            ListFiles(Directory.GetFiles(pathTextBox.Text));
+        }
+
+        private void EncryptFile(string inputFile)
+        {
+            File.Encrypt(inputFile);
+        }
+
+        private void DecryptFile(string inputFile)
+        {
+            File.Decrypt(inputFile);
+        }
+    }
+}
